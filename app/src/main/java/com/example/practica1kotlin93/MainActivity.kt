d3d2ecc0834cd62060342ac228206b917433643f
Braulio Ricardo Lopez Chavez <braulio20001@gmail.com>
2023-05-09 20:42:45 -0700

    Iniciando la app Android con Kotlin

---
+++ app/src/main/java/com/example/practica1kotlin93/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.practica1kotlin93
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var btnSaludar:Button;
+    private lateinit var txtNombre:EditText;
+    private lateinit var lblSaludar:TextView;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btnSaludar = findViewById(R.id.btnSaludo) as Button;
+        txtNombre = findViewById(R.id.txtSaludo) as EditText;
+        lblSaludar = findViewById(R.id.lblSaludo) as TextView;
+
+        btnSaludar.setOnClickListener {
+            var str:String
+            if(txtNombre.text.toString().contentEquals("")){
+                Toast.makeText(applicationContext,"Falto capturar datos",Toast.LENGTH_LONG).show()
+            }
+            btnSaludar.setOnClickListener {
+                var str: String
+                if(txtNombre.text.toString().contentEquals("")){
+                    Toast.makeText(applicationContext,"Falto capturar datos",Toast.LENGTH_LONG).show()
+                }
+                else {
+                    str = "Hola " + txtNombre.text.toString() + " ¿Cómo estás?";
+                    lblSaludar.text = str;
+                }
+            }
+        }
+
+            }
+
+    }
